cbb6e1dcdd952c88a4856beb855978d9dcc152cb
koyamal <tanakamaruko1121n@gmail.com>
2022-07-30 13:08:55 +0900

    Add data input form in data input page

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
@@ -7,6 +13,11 @@ app.get('/', (req, res) => {
 
 app.get('/datainput', (req, res) =>{
     res.render('datainput.ejs');
+});
+
+app.post('/datainput', (req, res) =>{
+    console.log(req.body);
+    res.render('index.ejs');
 });
 
 app.listen(3000);
