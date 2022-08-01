4d285b17d94cefc47c384849ba91577be2c85edb
koyamal <tanakamaruko1121n@gmail.com>
2022-08-01 20:46:48 +0900

    Add data to fig

--- public/js/fig.js
+++ public/js/fig.js
@@ -1,14 +1,23 @@
 console.log('hello');
 console.log(activityList);
+const labels = [];
+const times = [];
+
+for(let i = 0; i < activityList.length; i++){
+    labels.push(`${activityList[i]['month']}/${activityList[i]['date']}`);
+    times.push((activityList[i]['hour'] * 60 * 60 + activityList[i]['min'] * 60 + activityList[i]['sec'])/activityList[i]['dist']);
+}
 
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["6/24", "6/25", "6/26"],
+        labels: labels,
+        //labels: ["6/24", "6/25", "6/26"],
         datasets: [{
             label: 'タイム',
-            data: [25, 26, 27],
+            data: times,
+            // data: [25, 26, 27],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
             ],
