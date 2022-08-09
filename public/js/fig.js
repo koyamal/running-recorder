61eceda62dca31850966c372f7f6e3c3f662dbd4
koyamal <tanakamaruko1121n@gmail.com>
2022-08-09 21:28:22 +0900

    Add heart beat value into fig

--- public/js/fig.js
+++ public/js/fig.js
@@ -2,6 +2,7 @@ console.log('hello');
 console.log(activityList);
 const labels = [];
 const times = [];
+const beats = [];
 
 for(let i = 0; i < activityList.length; i++){
     if(activityList[i]['date'] !== '230'){
@@ -11,32 +12,50 @@ for(let i = 0; i < activityList.length; i++){
         const data = min + 0.01 * sec;
         labels.push(`${activityList[i]['month']}/${activityList[i]['date']}`);
         times.push(data);
+        beats.push(activityList[i]['hb_ave']);
     // times.push((activityList[i]['hour'] * 60 * 60 + activityList[i]['min'] * 60 + activityList[i]['sec'])/activityList[i]['dist']);
     }
 }
 times.push(null);
 labels.push('7/19');
 times.push(5.1);
+beats.push(0.2);
 labels.push('7/20');
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: labels,
-        datasets: [{
-            label: 'タイム',
-            data: times,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-            ],
-            showLine: false,
-            pointRadius: 5,
-            pointHoverRadius: 7,
-            borderWidth: 1
-        }],
+        datasets: [
+            {
+                label: 'タイム',
+                data: times,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                ],
+                showLine: false,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                borderWidth: 1
+            },
+            {
+                label: '心拍数',
+                data: beats,
+                backgroundColor: [
+                    'rgb(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgb(54, 162, 235, 1)',
+                ],
+                showLine: false,
+                pointRadius: 5,
+                pointHoverRadius: 7,
+                borderWidth: 1
+            }
+        ],
     },
     options: {
         scales: {
